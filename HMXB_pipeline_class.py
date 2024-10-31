ad60e03998dbf6f12c2ebb7830041b4dc5e09c96
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2024-10-31 11:03:09 +0100

    Version 2 Pipeline
    20243110
    
    Changelog:
    1. Added 'transform_pm'
    To transform proper motion to galactic frame independent of distance
    New pipeline can use both 'make_sky' and 'transform_pm' to calculate proper motion in galactic
    
    2. New test file GAIA_HMXB_DNE.ecsv
    DNE means DO NOT EDIT  I MEAN IT 
    use ecsv format to preserve units

--- HMXB_pipeline_class.py
+++ HMXB_pipeline_class.py
@@ -3,7 +3,8 @@
 """
 Created on Thu Oct 24 14:39:14 2024
 
-@author: karan
+@author: Karan Kumar 
+14906619
 """
 
 import numpy as np
@@ -27,8 +28,8 @@ class pipeline:
      # define constant
      self.RUWE = 1.4
     # self.list_ID = list_ID
-     self.R0= 8.5
-     self.sun_curve = 220
+     self.R0= 8.5 # solar galactic distance to center
+     self.sun_curve = 220 #km/s galactic velocity of the sun
      self.ra_np = np.radians(192.25) # deg to radians right acsension of north pole
      self.dec_np = np.radians(27.4) # deg to radians decliatoin of north pole
      self.theta_o = np.radians(123) # deg to radiansradians
@@ -40,18 +41,23 @@ class pipeline:
      self.U_sun = 10.8
      self.V_sun = 13.6
      self.W_sun = 7.6
-     self.table_name = table_name
+     self.table_name = table_name #string
      self.fmt = fmt
     def make_table(self):
         table = ascii.read(self.table_name,format=self.fmt)
         return table
     def modify_parllax_add_distance(self,table):
+        '''Calculate the distance as 1/parallax, rememeber its an estimator
+        add parallax error to negative parallax to get lower limit on distance
+        some parallax still may be negative'''
         #offset Parallax
-        p_offset = 0.0 # from GAIA EDR3
+        p_offset = 0.00 # from GAIA EDR3
         table['parallax'] = (table['parallax'] - p_offset)
         table['parallax'].unit = u.mas# from GAIA EDR3
         #add parallax units
-     
+        #adjust the negative values
+        negative_prlx_mask = table['parallax'] <=0 
+        table['parallax'][negative_prlx_mask] += table['parallax_error'][negative_prlx_mask]
         #add the distance to the table
         table['distance']= (1/table['parallax'])*u.kpc # kpc
         
@@ -72,7 +78,7 @@ class pipeline:
         long_rad = np.radians(long) #convert to radians
         lat_rad = np.radians(lat)
         obj_dist = table['distance'] # kpc
-        R_sqrd = self.R0**2 + (obj_dist**2 * np.cos(lat_rad)**2) - 2*self.R0*obj_dist*np.cos(long_rad)*np.cos(lat_rad)
+        R_sqrd = self.R0**2 + (obj_dist**2 * np.cos(lat_rad)**2) - 2*(self.R0*obj_dist*np.cos(long_rad)*np.cos(lat_rad))
         galactic_dist = np.sqrt(R_sqrd)
        # table.add_column(galactic_dist, name = 'galactic distance')
         table['galactic distance'] = galactic_dist*u.kpc
@@ -122,14 +128,40 @@ class pipeline:
         dec= table['dec']#deg
         pmra= table['pmra'] #mas/yr
         pmdec = table['pmdec']
-        distance = table['distance'] #kpc
-        sky = SkyCoord(ra = ra* u.deg, dec =dec *u.deg, pm_ra_cosdec= pmra*u.mas/u.yr, pm_dec=pmdec*u.mas/u.yr, distance=distance*u.kpc, frame='icrs')
+       # distance = table['distance']
+        #skycoord doesn't like ra = ra*u.deg, predefine the units somewhere else
+        sky = SkyCoord(ra = ra, dec =dec, pm_ra_cosdec= pmra, pm_dec=pmdec, frame='icrs')
         galactic= sky.transform_to('galactic')
         pm_l = galactic.pm_l_cosb
         pm_b = galactic.pm_b
         table['pm_l'] = pm_l
         table['pm_b'] = pm_b
         return table
+    def transform_pm(self,table):
+        '''Poleski'''
+        dec = table['dec']
+        ra = table['ra']
+        ra_rad = np.radians(ra)
+        dec_rad = np.radians(dec)
+        pmra = table['pmra']
+        pmdec = table['pmdec']
+
+        C1 = np.sin(self.dec_np)*np.cos(dec_rad) - np.cos(self.dec_np)*np.sin(dec_rad)*np.cos(ra_rad - self.ra_np)
+        C2 = np.cos(self.dec_np)*np.sin(ra_rad - self.ra_np)
+        cosb = np.sqrt(C1**2 + C2**2)
+        #C_mtrx = 1/(cosb)*np.array([C1, C2],[-C2, C1])
+        
+        mu_l_cosb = (1/cosb)* (C1 * pmra + C2 *pmdec)
+        mu_b = (1/cosb) * (-C2 * pmra + C1*pmdec)
+        
+        table['pm_l_poleski'] = mu_l_cosb 
+        table['pm_b_poleski'] = mu_b 
+        #set units
+        table['pm_l_poleski'].unit = u.mas/u.yr
+        table['pm_b_poleski'].unit = u.mas/u.yr
+        
+        return table  
+
     def transform_space_velocity(self,ra, dec):
         '''Johnson 1986 calculate space velocities
         Use a transformation Matrix to convert ra and dec corrdinates into space velocity components
@@ -168,7 +200,7 @@ class pipeline:
         return B
     
     def calculate_space_velocity(self,table):
-        '''Caulate the space velocity of a star WRT the local standard of rest, subtracting solar motion'''
+        '''Caulate the space velocity of a star WRT the local standard of rest, subtracting solar motion Johnson 1986'''
     
         UVW = []
         for row in table:
@@ -198,7 +230,7 @@ class pipeline:
         return table
     def solar_proper_motion(self,table):
         '''Calculate the proper motion compoent due to solar motion in the galactic plane
-        '''
+        moffat 1998'''
         lat = table['b']
         long = table['l']
         dist = table['distance']
@@ -206,8 +238,8 @@ class pipeline:
         long_rad = np.radians(long)
         lat_rad = np.radians(lat)
         
-        Kr_mul_sol = self.U_sun*np.sin(long_rad) - self.V_sun*np.cos(lat_rad)
-        table['mu_l_sol'] = (Kr_mul_sol/(self.k*dist)) # need to specifiy units to subtract from proper motion
+        Kr_mul_sol = self.U_sun*np.sin(long_rad) - self.V_sun*np.cos(long_rad)
+        table['mu_l_sol'] = (Kr_mul_sol/(self.k*dist))
         table['mu_l_sol'].unit = u.mas/u.yr
         
         Kr_mub_sol = self.U_sun*np.cos(long_rad)*np.sin(lat_rad) + self.V_sun*np.sin(long_rad)*np.sin(lat_rad) - self.W_sun*np.cos(lat_rad)
@@ -228,32 +260,67 @@ class pipeline:
         long_rad = np.radians(long)
         lat_rad = np.radians(lat)
         #calculate proper motion due to galactic rotation
-        K_mul_rot = ((self.R0/(dist*np.cos(lat_rad))) *(omega-omega_0)*np.cos(long_rad))-omega
+        K_mul_rot = ((self.R0/(dist*np.cos(lat_rad)))*(omega-omega_0)*np.cos(long_rad))-omega
         table['mu_l_rot'] = K_mul_rot/(self.k)
         table['mu_l_rot'].unit = u.mas/u.yr
         
         #do the same for latitudal proper motion
-        K_mub_rot = -(self.R0 /dist)*(omega-omega_0)*np.sin(lat_rad)*np.sin(long_rad)
+        K_mub_rot = -(self.R0/dist)*(omega-omega_0)*np.sin(lat_rad)*np.sin(long_rad)
         table['mu_b_rot'] = K_mub_rot/self.k
         table['mu_b_rot'].unit = u.mas/u.yr
-        #ADD TO TABLE
-        #table.add_column(mul_rot,name='pm_l_rot')
-        #table.add_column(mub_rot,name='pm_b_rot')
+
         return table
-    def lay_pipe(self,table,filename,filetype):
-        table= self.modify_parllax_add_distance(table)
-        table = self.make_sky(table)
+    def peculiar_velocity(self, table):
+        '''Calculate peculiar velocity of a star based on its proer motion 
+        Follows Moffat 1998 
+        account for peculiar motion as observed motion minus expeected motion
+        account for solar bias
+        See solar_proper_motion
+        See flat_rotation_curve
+        returns peculiar velocity in km/s
+        works now'''
+        long = table['l'] # deg
+        lat = table['b'] #deg
+        long_rad =  np.radians(long)
+        lat_rad = np.radians(lat)
+        dist = table['distance'] # kpc
+        # observed proper Motions
+        obs_mu_l = table['pm_l_poleski']
+        obs_mu_b = table['pm_b_poleski']
+        #motion due to galactic rotation 
+        
+        rotation_mu_l = table['mu_l_rot']
+        rotation_mu_b = table['mu_b_rot']
+        
+        #calculate solar motion for each object 
+        mul_sol = table['mu_l_sol']
+        mub_sol = table['mu_b_sol']
+    
+        pec_mu_l = obs_mu_l - mul_sol - rotation_mu_l
+        pec_mu_b = obs_mu_b - mub_sol - rotation_mu_b
+        print(pec_mu_l, pec_mu_b)
+        #convert to km/s
+        V_pec = self.k*dist*np.sqrt(pec_mu_l**2 + pec_mu_b**2)
+        table['Peculiar Velocity'] = V_pec
+        table['Peculiar Velocity'].unit = u.km/u.s
+        return table
+    def lay_pipe(self,filename,filetype):
+        '''Combine everything'''
+        table = self.make_table()
+        table = self.modify_parllax_add_distance(table)
+        table =self.transform_pm(table)
+        #table = self.make_sky(table)
         table = self.Galaxy_dist(table)
-        table= self.rotation_curve(table)
+        table = self.rotation_curve(table)
         table = self.vlsr_model(table)
         #table = self.calculate_space_velocity(table)
         table = self.solar_proper_motion(table)
         table = self.flat_rotation_curve(table)
-        
+        table = self.peculiar_velocity(table)
         table.write(filename, format=filetype,overwrite=True)
         return table
-test_table = "Test_XRBCATS.csv"
+test_table = "GAIA_HMXB_DNE.ecsv"
 if __name__ == "__main__":
-    test_massive = pipeline(test_table,fmt='csv')
+    test_massive = pipeline(test_table,fmt='ecsv')
     my_table = test_massive.make_table()
-    x = test_massive.lay_pipe(my_table, filename='test_lay_pipe.ecsv',filetype='ascii.ecsv')
+    x = test_massive.lay_pipe(filename='test_lay_pipe.ecsv',filetype='ascii.ecsv')
